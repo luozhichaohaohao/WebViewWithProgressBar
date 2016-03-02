3b077b23c7be78049bfd982d805df4986543af60
lzc <191671412@qq.com>
2016-03-02 15:26:36 +0800

    fix some bug and make a tag

--- app/src/main/java/com/llt/park/activities/MainActivity.java
+++ app/src/main/java/com/llt/park/activities/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView _text = new TextView(this);
-        _text.setText("Text"+"branch_dev_lzc");
+        _text.setText("Text"+"branch_dev_lzc"+"fix some bug and make a tag");
         setContentView(_text);
     }
 }
