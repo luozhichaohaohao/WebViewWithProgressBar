dda61c9c8856d9cf6362653ecc69aa04e393c37d
lzc <191671412@qq.com>
2016-03-02 14:28:44 +0800

    push origin code

---
+++ app/src/main/java/com/llt/park/AppApplication.java
@@ -0,0 +1,14 @@
+package com.llt.park;
+
+import android.app.Application;
+
+/**
+ * Created by a on 2016/3/1.
+ */
+public class AppApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/llt/park/activities/MainActivity.java
@@ -0,0 +1,18 @@
+package com.llt.park.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by a on 2016/3/1.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView _text = new TextView(this);
+        _text.setText("Text");
+        setContentView(_text);
+    }
+}
